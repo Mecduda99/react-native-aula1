8029f3ed87622681a3c0703cf9bdebdca0050a20
mecduda99 <rm98760@fiap.com.br>
2024-02-28 23:44:38 -0300

    primeiro commit - aprendendo o que é view

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.frase}>"Nunca espere que as pessoas reconheçam o quanto você andou, essa tarefa é sua, não delas".</Text>
+      <Text style={styles.quote}>@habitosquemudam</Text>
+      <Text style={styles.nome}>Maria Eduarda Costa de Araújo Vieira - RM 98760</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +15,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F5F5DC',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  frase: {
+    color: '#A52A2A',
+    fontSize: 20,
+    fontWeight: 'bold',
+    margin: 20,
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+  quote: {
+    textAlign: 'right',
+    paddingBottom: 20,
+    fontSize: 12,
+    fontStyle: 'italic',
+  },
+  nome: {
+    marginTop: 20,
+  }
 });
